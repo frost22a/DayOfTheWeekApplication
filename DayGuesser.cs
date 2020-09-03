77e62ee4aa3fef3534ce94770a7768dac824b697
Przemek Mroz <frost22a@gmail.com>
2020-09-03 13:48:02 +0200

    upload application

---
+++ DayGuesser.cs
@@ -0,0 +1,42 @@
+﻿using DayOfTheWeekApp.Core;
+using System;
+
+namespace DayOfTheWeekApp
+{
+    public class DayGuesser
+    {
+        public DayCalculator Calculator { get; set; }
+
+        public DateTimeOffset UserDateOfBirth { get; set; }
+
+        public void IntroduceTheApplication() 
+        {
+            Console.WriteLine("Witam, jestem botem który potrafi wyliczyć dzień tygodnia, w którym się " +
+                "urodziłeś na podstawie Twojej daty urodzin.");
+        }
+
+        public void AskUserForTheirDateOfBirth() 
+        {
+            Console.WriteLine("Podaj mi proszę swoją datę urodzenia:");
+            var userDate = Console.ReadLine();
+
+            var  succesed = DateTimeOffset.TryParse(userDate, out var date);
+            if (succesed)
+            {
+                UserDateOfBirth = date;
+                return;
+            }
+            Console.WriteLine("Zły format, podaj datę w formie mm/dd/yyyy:");
+            AskUserForTheirDateOfBirth();
+    }
+
+    public void CalculateDayOfTheWeek() 
+        {
+        
+        }
+        public void PrintDayOfTheWeek()
+        {
+            Console.WriteLine("Urodziłeś się w " + "");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DayOfTheWeekApp.Core;
+using System;
+
+namespace DayOfTheWeekApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var guesser = new DayGuesser();
+            guesser.IntroduceTheApplication();
+            guesser.AskUserForTheirDateOfBirth();
+            guesser.CalculateDayOfTheWeek();
+            guesser.PrintDayOfTheWeek();
+        }
+    }
+}
